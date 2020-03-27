ac52d1713f0eaee46af8190af2f416d87cff8c41
phoenixmcknight <phoenixmcknight@gmail.com>
2020-03-27 19:11:32 -0400

    adds getRecipe function to return recipes from SpoonAPI

--- Eat_At_Home/Networking/SpoonApiClient.swift
+++ Eat_At_Home/Networking/SpoonApiClient.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class SpoonAPIClient {
+    static let client = SpoonAPIClient()
+    
+    func getRecipes(query keyWord:String,cuisine:String,diet:String,excludeIngredients:String,intolerances:String,includeIngredients:String,type course:String,maxReadyTime:Int,maxCalories:Int, completionHandler:@escaping(Result<[Recipe],AppError>)-> Void) {
+    
+        //change &number when you finish testing
+        let urlStr = "https://api.spoonacular.com/recipes/complexSearch?apiKey=\(Secrets.spoonApiKey)&instructionsRequired=true&author&number=10\(keyWord)\(cuisine)\(diet)\(excludeIngredients)\(includeIngredients)\(intolerances)\(course)\(maxReadyTime)\(maxCalories)"
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+                
+            case.success(let recipeData):
+                do {
+                    let recipeArray = try Recipe.getRecipeData(data: recipeData)
+                    completionHandler(.success(recipeArray ?? []))
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+    }
+}
