91b902be621e3e7f72e67c895eb41628b7debb7f
phoenixmcknight <phoenixmcknight@gmail.com>
2020-03-27 19:12:51 -0400

    creates structs in SpoonAPIModel to receive data from spoonAPI

--- Eat_At_Home/Networking/SpoonApiModel.swift
+++ Eat_At_Home/Networking/SpoonApiModel.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+// MARK: - RecipeWrapper
+struct RecipeWrapper: Codable {
+    let results: [Recipe]
+    let number,totalResults: Int
+}
+
+// MARK: - Result
+struct Recipe: Codable {
+    let vegetarian, vegan, glutenFree, dairyFree: Bool
+    let sourceUrl: String
+    let spoonacularSourceUrl: String
+    let sourceName: String?
+    let pricePerServing: Double
+    let id: Int
+    let title: String
+    let readyInMinutes, servings: Int
+    let image: String
+    let imageType: ImageType
+    let summary: String
+    let cuisines, dishTypes, diets: [String]
+    let analyzedInstructions: [AnalyzedInstruction]
+    let preparationMinutes, cookingMinutes: Int?
+    
+    static func getRecipeData(data:Data)throws -> [Recipe]? {
+             do{
+                 let recipeData = try
+                     JSONDecoder().decode(RecipeWrapper.self, from: data)
+                return recipeData.results
+             } catch {
+                 print(error)
+                 return nil
+             }
+         }
+
+}
+
+// MARK: - AnalyzedInstruction
+struct AnalyzedInstruction: Codable {
+    let name: String
+    let steps: [Step]
+}
+
+// MARK: - Step
+struct Step: Codable {
+    let number: Int
+    let step: String
+}
+
+enum ImageType: String, Codable {
+    case jpeg = "jpeg"
+    case jpg = "jpg"
+}
