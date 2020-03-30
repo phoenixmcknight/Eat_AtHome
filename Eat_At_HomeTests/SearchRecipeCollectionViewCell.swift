2790160248d539565f51a4d5b95f18bc3707b8f1
phoenixmcknight <phoenixmcknight@gmail.com>
2020-03-29 23:09:37 -0400

    adds collectionView for dish and cuisine collectionviews

--- Eat_At_HomeTests/SearchRecipeCollectionViewCell.swift
+++ Eat_At_HomeTests/SearchRecipeCollectionViewCell.swift
@@ -8,6 +8,75 @@
 
 import UIKit
 
-class SearchRecipeCollectionViewCell: UICollectionViewCell {
+class SearchRecipeCourseAndCuisineCollectionViewCell: UICollectionViewCell {
+    lazy var foodImageView:UIImageView =
+        {
+         let image = UIImageView()
+            image.contentMode = .scaleToFill
+           
+            image.layer.shadowColor = UIColor.black.cgColor
+            image.layer.shadowRadius = 5.0
+            image.layer.shadowOpacity = 10
+            return image
+    }()
+    
+    lazy var foodLabel:UILabel =
+        {
+            let label = UILabel()
+            label.textAlignment = .center
+            label.adjustsFontSizeToFitWidth = true
+            label.numberOfLines = 0
+            label.font = UIFont(name: StyleGuide.FontStyle.fontName, size: StyleGuide.FontStyle.fontSize)
+            label.textColor = StyleGuide.FontStyle.fontColor
+            return label
+    }()
+    
+      required init?(coder: NSCoder)
+      {
+          fatalError("init(coder:) has not been implemented")
+      }
+      
+      override init(frame: CGRect)
+      {
+          super.init(frame: frame)
+        commonInit()
+      }
+    
+    private func commonInit()
+    {
+        addSubviews()
+        foodImageConstraints()
+        foodLabelConstraints()
+        self.backgroundColor = StyleGuide.AppColors.backgroundColor
+    }
+    
+    private func addSubviews()
+    {
+        self.addSubview(foodImageView)
+        self.addSubview(foodLabel)
+    }
+    
+    private func foodImageConstraints()
+    {
+        foodImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            foodImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            foodImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            foodImageView.widthAnchor.constraint(equalTo: self.widthAnchor),
+            foodImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.7)
+            
+        ])
+    }
+    
+    private func foodLabelConstraints()
+    {
+        foodLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            foodLabel.topAnchor.constraint(equalTo: foodImageView.bottomAnchor),
+            foodLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            foodLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            foodLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
     
 }
