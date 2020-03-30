1c97d43d61ca2a1d06f1aa6f0790a06cee2d342e
phoenixmcknight <phoenixmcknight@gmail.com>
2020-03-30 17:44:21 -0400

    creates cell for diet CollectionView

--- Eat_At_Home/Views/DietCollectionViewCell.swift
+++ Eat_At_Home/Views/DietCollectionViewCell.swift
@@ -10,4 +10,109 @@ import UIKit
 
 class DietCollectionViewCell: UICollectionViewCell {
     
+    var dietIsSelected:Bool = false {
+           didSet {
+               switchBackgroundColorOfCell(bool: dietIsSelected)
+           }
+       }
+    
+    var currentOption:String = ""
+   
+    lazy var dietImageView:UIImageView =
+          {
+           let image = UIImageView()
+              image.contentMode = .scaleToFill
+             
+              image.layer.shadowColor = UIColor.black.cgColor
+              image.layer.shadowRadius = 5.0
+              image.layer.shadowOpacity = 10
+              return image
+      }()
+      
+      lazy var dietFoodLabel:UILabel =
+          {
+              let label = UILabel()
+              label.textAlignment = .center
+              label.adjustsFontSizeToFitWidth = true
+              label.numberOfLines = 0
+              label.font = UIFont(name: StyleGuide.FontStyle.fontName, size: StyleGuide.FontStyle.fontSize)
+              label.textColor = StyleGuide.FontStyle.fontColor
+              return label
+      }()
+    
+    lazy var dietActivityIndicator:UIActivityIndicatorView =
+        {
+      let activity = UIActivityIndicatorView()
+            activity.hidesWhenStopped = true
+            activity.contentMode = .center
+            activity.style = .medium
+            return activity
+        }()
+   
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        addSubviews()
+        dietImageViewConstraints()
+        dietLabelConstraints()
+        activityIndicatorConstraints()
+    }
+    
+    
+    private func addSubviews() {
+        self.addSubview(dietImageView)
+        self.addSubview(dietFoodLabel)
+        self.addSubview(dietActivityIndicator)
+    }
+    
+    private func dietLabelConstraints()
+    {
+        dietFoodLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dietFoodLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            dietFoodLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+         dietFoodLabel.leadingAnchor.constraint(equalTo: dietImageView.trailingAnchor),
+         dietFoodLabel.heightAnchor.constraint(equalTo: dietImageView.heightAnchor)
+        ])
+    }
+    
+    private func dietImageViewConstraints()
+    {
+        dietImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        dietImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+        dietImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+        dietImageView.heightAnchor.constraint(equalTo: self.heightAnchor),
+        dietImageView.widthAnchor.constraint(equalTo: self.widthAnchor,multiplier: 0.3)
+        ])
+        
+    }
+    private func activityIndicatorConstraints() {
+        dietActivityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            dietActivityIndicator.centerXAnchor.constraint(equalTo: dietImageView.centerXAnchor),
+            dietActivityIndicator.centerYAnchor.constraint(equalTo: self.dietImageView.centerYAnchor)
+        ])
+    }
+    
+    private func switchBackgroundColorOfCell(bool:Bool) {
+        switch bool {
+        case true:
+            self.layer.borderWidth = 10
+            self.layer.borderColor = UIColor.green.cgColor
+           
+        case false:
+            self.layer.borderWidth = 0
+            self.layer.borderColor = .none
+        }
+    }
 }
