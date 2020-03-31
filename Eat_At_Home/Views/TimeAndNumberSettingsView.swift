b3413151166f37ac267c6cf1ff312f36268b7687
phoenixmcknight <phoenixmcknight@gmail.com>
2020-03-31 17:18:05 -0400

    creates constraints for timeandNumberSettingsView

--- Eat_At_Home/Views/TimeAndNumberSettingsView.swift
+++ Eat_At_Home/Views/TimeAndNumberSettingsView.swift
@@ -10,22 +10,46 @@ import UIKit
 
 class TimeAndNumberSettingsView: UIView {
 
-    lazy var readyTableView:UITableView = {
+    lazy var timeAndNumberTableView:UITableView = {
         let tableView = UITableView()
+        tableView.backgroundColor = StyleGuide.AppColors.backgroundColor
+               
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "generic")
+              
+        tableView.backgroundColor = StyleGuide.AppColors.backgroundColor
         return tableView
     }()
-    
-    lazy var numberPicker:UIPickerView = {
-        let picker = UIPickerView()
-        return picker
-    }
-    
+     
     override init(frame:CGRect) {
         super.init(frame:frame)
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    private func commonInit() {
+        addSubviews()
+        timeAndNumberTableViewConstraints()
+        self.backgroundColor = StyleGuide.AppColors.backgroundColor
+    }
+    private func addSubviews() {
+        self.addSubview(timeAndNumberTableView)
+    }
+   
+    private func timeAndNumberTableViewConstraints() {
+        timeAndNumberTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            timeAndNumberTableView.topAnchor.constraint(equalTo: self.topAnchor,constant: UIScreen.main.bounds.height * 0.01),
+            timeAndNumberTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: UIScreen.main.bounds.height * 0.01),
+            timeAndNumberTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -UIScreen.main.bounds.height * 0.01),
+            timeAndNumberTableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+
 }
+
+
