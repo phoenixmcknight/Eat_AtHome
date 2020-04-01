9481d1859085c658d88e6606643b05072d556532
phoenixmcknight <phoenixmcknight@gmail.com>
2020-04-01 03:22:12 -0400

    adds DetailVC and reverts prior merge

---
+++ Eat_At_Home/Models/DetailRecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Eat_At_Home
+//
+//  Created by Phoenix McKnight on 4/1/20.
+//  Copyright © 2020 Phoenix McKnight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Eat_At_Home/Models/SpoonAPIModel.swift
@@ -0,0 +1,9 @@
+//
+//  SpoonAPIModel.swift
+//  Eat_At_Home
+//
+//  Created by Phoenix McKnight on 4/1/20.
+//  Copyright © 2020 Phoenix McKnight. All rights reserved.
+//
+
+import Foundation
