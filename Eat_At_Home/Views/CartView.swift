15c541134913c6a1d645c9315f79adb51ed716de
phoenixmcknight <phoenixmcknight@gmail.com>
2020-04-01 09:25:49 -0400

    adds constraints to cartView

--- Eat_At_Home/Views/CartView.swift
+++ Eat_At_Home/Views/CartView.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class CartView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+   lazy var cartTable: UITableView = {
+           let tableview = UITableView()
+           tableview.backgroundColor = StyleGuide.AppColors.backgroundColor
+           tableview.separatorColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    tableview.register(CartTableViewCell.self, forCellReuseIdentifier: RegisterCollectionViewCells.cart.rawValue)
+          
+           return tableview
+          }()
+    lazy var customActivityIndictator = CustomIndictator(frame: .zero)
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func commonInit() {
+        addSubviews()
+        tableViewConstraints()
+        customActivityIndictator.setToCenter(view: self, sizeRelativeToView: 0.2)
+    }
+    private func addSubviews() {
+        self.addSubview(cartTable)
+    }
+    
+    private func tableViewConstraints() {
+        cartTable.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cartTable.topAnchor.constraint(equalTo: self.topAnchor),
+            cartTable.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            cartTable.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            cartTable.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
 
 }
